18355fc3faf9d2d543de906bd5c8ffe5b0a90592
Sairaj-p <sairaj.patil124@nmims.edu.in>
2023-07-30 19:45:48 +0530

    update

--- model_2.py
+++ model_2.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 import pandas as pd
@@ -19,19 +20,23 @@ def train_model(model, data, targets):
 # #Logistic Regression model
 
 
-# y_pred=log_reg.predict(X_test)
-# log_reg_accuracy = accuracy_score(Y_test, y_pred)
-# print('Accuracy logistic regression: ', log_reg_accuracy,'\n')
 
 def analys(text):
     data = pd.read_csv("clean_data.csv")
     Xfeatures = data["clean-text"]
     Ylabels = data["sentiment"]
+    ly = LabelEncoder()
+    y = ly.fit_transform(Ylabels)
+    print(ly.classes_)
     cv = CountVectorizer()
     X =cv.fit_transform(Xfeatures.values.astype('U'))
     X.toarray()
     X_train,X_test,Y_train,Y_test = train_test_split(Xfeatures.values.astype('U'),Ylabels,test_size=0.2,random_state=42)
     log_reg = train_model(LogisticRegression(solver='liblinear',random_state = 42), X_train, Y_train)
+    y_pred=log_reg.predict(X_test)
+    log_reg_accuracy = accuracy_score(Y_test, y_pred)
+    print('Accuracy logistic regression: ', log_reg_accuracy,'\n')
+
     def final_predict(text):
         lr = list(log_reg.predict(text))
         return lr
